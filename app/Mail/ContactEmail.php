be4fbb2e50f45ea22dfd34fca6e50664cd61e4b4
4maaz5 <104651321+4maaz5@users.noreply.github.com>
2024-05-02 10:06:05 +0500

    Queue-implementation

--- app/Mail/ContactEmail.php
+++ app/Mail/ContactEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ContactEmail extends Mailable
+class ContactEmail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
